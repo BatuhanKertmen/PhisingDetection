f303fb89b4a1f8568704b3f504fadb427dc192ff
doguozcan <76822280+doguozcan@users.noreply.github.com>
2022-04-13 21:49:36 +0300

    Update selenium_deneme.py

--- selenium_deneme.py
+++ selenium_deneme.py
@@ -4,7 +4,7 @@ os.environ["PATH"] += os.pathsep + r'C:\Users\D\geckodriver'
 from selenium import webdriver
 import time
 browser = webdriver.Firefox()
-browser.get("https://www.nihanozcan.com")
+browser.get("https://www.google.com")
 
 time.sleep(15)
 
