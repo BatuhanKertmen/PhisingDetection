2e46f98c9820bb7c970f1c6d9da1b4c2531a8adb
Batuhan Kertmen <60521508+BatuhanKertmen@users.noreply.github.com>
2022-04-16 15:30:12 +0300

    valid domains stored in a file and restructring of the code

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,6 @@
 import json
 import os
-
+from itertools import repeat
 from requests.exceptions import TooManyRedirects, ConnectionError, ReadTimeout
 
 import matplotlib.pyplot as plt
@@ -8,41 +8,25 @@ import DownloadDomains
 import GetContents
 import time
 import subprocess  # in order to speed up processes, subprocess module is going to be used
-import pathlib
 from ping3 import ping
 import concurrent.futures
 from joblib import Parallel, delayed
+import multiprocessing
+from paths import VALID_NAMES_TXT
 
-import directories as dir
 
-WHO_IS_URL = "https://www.whoisds.com/newly-registered-domains"
-
-zip_link = DownloadDomains.ScrapTodaysDomainsFileLink(WHO_IS_URL)
-domains_address = DownloadDomains.DownloadDomainList(zip_link)
-
-domain_file = open(domains_address, 'r')
-
-domain_names = domain_file.readlines()[:10]
-valid_domain_names = []
-
-print("1")
-
-def pinging(d_name):
+def pinging(d_name, queue):
     if ping(d_name[:-1]):
-        valid_domain_names.append(d_name[:-1])
+        queue.put(d_name)
 
 
-print("2")
-tic = time.perf_counter()
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
-    executor.map(pinging, domain_names[:1000])
-
-toc = time.perf_counter()
-print("3")
-
-print('Process finished in {} seconds.'.format(toc - tic))
-print("Number of valid domain names is {}".format(len(valid_domain_names)))
+def writer(dest_filename, some_queue, some_stop_token):
+    with open(dest_filename, 'w') as dest_file:
+        while True:
+            line = some_queue.get()
+            if line == some_stop_token:
+                return
+            dest_file.write(line)
 
 
 def scrape(domain_name, working_directory_path):
@@ -72,18 +56,52 @@ def scrape(domain_name, working_directory_path):
         #print(domain_name, "exceeded 30 redirections, skipping!")
 
 
-results = {}
-print("----------------------------- BEGINNING --------------------------------------")
-try:
-    for n_job_count in range(1000, 50001, 250):
-        start = time.perf_counter()
-        thread = Parallel(n_jobs=n_job_count, prefer="threads")((delayed(scrape)(i, str(dir.WORKING_DIR)) for i in valid_domain_names))
-        end = time.perf_counter()
-        print(end - start)
-        results[n_job_count] = end - start
-finally:
-    x_val = results.keys()
-    y_val = results.values()
+if __name__ == "__main__":
 
-    plt.plot(x_val, y_val)
-    plt.show()
+    WHO_IS_URL = "https://www.whoisds.com/newly-registered-domains"
+
+    zip_link = DownloadDomains.ScrapTodaysDomainsFileLink(WHO_IS_URL)
+    domains_address = DownloadDomains.DownloadDomainList(zip_link)
+
+    domain_file = open(domains_address, 'r')
+
+    domain_names = domain_file.readlines()[:10]
+
+    tic = time.perf_counter()
+
+    queue = multiprocessing.Queue()
+    STOP_TOKEN = "STOP!!!"
+
+    writer_process = multiprocessing.Process(target=writer, args=(VALID_NAMES_TXT, queue, STOP_TOKEN))
+    writer_process.start()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
+        executor.map(pinging, domain_names[:1000], repeat(queue))
+
+    queue.put(STOP_TOKEN)
+    writer_process.join()
+
+    toc = time.perf_counter()
+
+    print('Process finished in {} seconds.'.format(toc - tic))
+
+
+
+    results = {}
+    print("----------------------------- BENCHMARKING --------------------------------------")
+
+    exit()
+
+    try:
+        for n_job_count in range(1000, 50001, 250):
+            start = time.perf_counter()
+            #thread = Parallel(n_jobs=n_job_count, prefer="threads")((delayed(scrape)(i, str(dir.WORKING_DIR)) for i in valid_domain_names))
+            end = time.perf_counter()
+            print(end - start)
+            results[n_job_count] = end - start
+    finally:
+        x_val = results.keys()
+        y_val = results.values()
+
+        plt.plot(x_val, y_val)
+        plt.show()
