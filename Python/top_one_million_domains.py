a717454d33decb55449862ea33d1209e44099b53
doguozcan <doguozcan@sabanciuniv.edu>
2022-05-16 20:23:43 +0300

    Tranco crawling with Selenium

--- Python/top_one_million_domains.py
+++ Python/top_one_million_domains.py
@@ -1,5 +1,4 @@
 import pandas as pd
 
 df = pd.read_csv('top-1m.csv')
-top_one_millions_domains = [website for website in df['Website']]
-print(top_one_millions_domains)
+domains = [website for website in df['Website']]
