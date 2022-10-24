69436ccb915df805ac970978f94e7465b217010c
Batuhan <batuhankertmen@sabanciuniv.edu>
2022-10-24 20:29:18 +0300

    extra exception

--- main.py
+++ main.py
@@ -42,6 +42,8 @@ def scrape(domain_name, folder):
         Log.log("Max time exceeded " + domain_name + " skipping!")
     except TooManyRedirects:
         Log.log(domain_name + " exceeded 30 redirections, skipping!")
+    except Exception:
+        Log.warning("Unexpected error!")
 
 
 def extractFeatures(filename, tld):
